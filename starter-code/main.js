30c1300abb26433d34b30ca4276420bd55a11f5c
itsthecheat <llaurenb@gmail.com>
2016-10-16 22:55:26 -0400

    for loop and functions added

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,10 +5,24 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour && cardThree) {
+/* if (cardTwo === cardFour && cardThree) {
   alert('Sorry, try again.')
 }
 else if (cardOne === cardTwo || cardThree === cardFour) {
   alert('You found a match!')
+} */
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+
+    var cardElement = document.createElement('div');
+
+    cardElement.className = 'card';
+
+    board.appendChild(cardElement);
+  }
+
 }
 
